65e5cfa850af54d2d510b6c0646a45ff19bf0a34
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-17 21:11:42 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,11 +3,13 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailAddressParser
-  attr_accessor :e_add
-  def initialize(e_add)
-    @e_add=e_add
+  attr_accessor :e_add #allows to read and write for e_add
+  #This then allows for us to edit and read back the info without having to add
+  #a def for each value we might want e_mail or when wanting to change the value
+  def initialize(e_add)  #initializes new objects for EmailAddressParser int his case a string of email addresses
+    @e_add=e_add # takes the local variable and then
   end
   def parse
-    (e_add.split.collect {|eaddy|eaddy.split(",")}).flatten.uniq
+    (e_add.split.collect {|eaddy|eaddy.split(",")}).uniq.flatten
   end
 end
