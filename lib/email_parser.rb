50b19ab34fc5ba28c8262e6034273424ac01f445
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-17 21:34:00 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -15,7 +15,7 @@ class EmailAddressParser
     #this allow scope across the class variable EmailAddressParser and its new objects EmailAddressParser.new
   end
   def parse # This is to be called on our objects via "variable".parse
-    # (e_add.split.collect {|eaddy|eaddy.split(",")}).uniq.flatten
+    (e_add.split.collect {|eaddy|eaddy.split(",")}).uniq.flatten
     #^ We need to split the email by "," and " "
     #by using split without an arguement it takes away spaces by deafult
     #Then using collect we can collect the elements of e_addy and creat a new array
@@ -23,6 +23,11 @@ class EmailAddressParser
     # After the pipes we then use tell it what to do with those elements
     # We removed the spaces so now we simply have "," to removed
     # after removing the commas we still need to only have one copy of each eamil in the list so add ".uniq"
-    #
+    #However, now we have an array in an array. Split takes the string and makes it an array.
+    # collect then takes the array and puts it into another array.
+    #because the split is in the collect it reads the elements as a string but at the end "}" is put back into the new collect array in the array.
+    # therefor we need to use ".flatten" to make it one array.
+    # after that we need to put everything that is happening before .uniq.flatten in ()
+    #Because ruby reads all in parathesis first. This will then allow us to do everything in one line in correct order 
   end
 end
